1c08c6a37f7ca3ee0fc192fc00dc4b4c1e9f1e11
Pizzarules668 <Colin.Sutter1@gmail.com>
2024-02-04 15:04:12 -0600

    made bot

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Discord-Weather-Bot
 Basic discord bot that uses my python lib to get the weather
+
+## Usage
+- `/weather observation_station`: get the current conditions at the observation station
---
+++ main.py
@@ -0,0 +1,59 @@
+import disnake
+from disnake.ext import commands
+
+from dotenv import load_dotenv
+import os
+
+from NWS_Weather import current_weather
+
+intents = disnake.Intents.default()
+intents.message_content = True
+intents.guilds = True
+
+bot = commands.Bot(intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+@bot.slash_command(name="weather", description="Get weather from KPVB")
+async def weather(ctx, station: str = "KPVB"):
+    # if station does not start with K, add K to the beginning
+    if not station.startswith("K"): station = f"K{station}"
+    
+    station = station.upper()
+    
+    try:
+        weather = current_weather(station=station)[0]
+
+        # Create an embed
+        embed = disnake.Embed(
+            title="Current Weather",
+            description=f"Weather at {station}",
+            color=0x00ff00,
+            timestamp=weather.timestamp
+        )
+        
+        embed.set_thumbnail(url=weather.icon)
+        
+        # Add fields to the embed
+        # fields are time, temp, wind speed, wind direction, relative humidity, and cloud ceiling
+        
+        embed.add_field(name="Temperature",         value=f"{round((weather.temperature * 1.8) + 32)}°F",      inline=True)
+        embed.add_field(name="Wind Speed",          value=f"{round(weather.wind_speed * 0.6213711922)} MPH",   inline=True)
+        embed.add_field(name="Wind Direction",      value=f"{round(weather.wind_direction)}°",                 inline=True)
+        embed.add_field(name="Relative Humidity",   value=f"{round(weather.relative_humidity)}%",              inline=True)
+        
+        clouds = weather.cloud_layers.layers[0]
+        if clouds["amount"] == "CLR": embed.add_field(name="Cloud Ceiling", value="Clear", inline=True)
+        else: embed.add_field(name="Cloud Ceiling", value=f"{weather.cloud_layers.layers[0]['amount']} {round(weather.cloud_layers.layers[0]['base']['value'] * 3.2808)} ft",                inline=True)
+        
+        
+        # Send the embed
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.send("Not a valid airport (or something else went wrong)")
+        print(e)
+
+load_dotenv()
+bot.run(os.getenv("TOKEN"))
